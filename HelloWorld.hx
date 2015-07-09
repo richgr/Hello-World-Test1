a28a6c9081f7b3f55e02f45606827843793f0773
Graham Rich <richgr@gmail.com>
2015-07-08 22:06:12 -0300

    Bring it back!

--- HelloWorld.hx
+++ HelloWorld.hx
@@ -1,5 +1,5 @@
 class HelloWorld {
   static public function main():Void {
-    trace("Hello Haxe World.  This is a first test of Haxe and Git.  Test 1...2...3!");
+    trace("Hello Haxe World.  This is a first test of Haxe and Git.  Test 1...2...3!  Bring it back!");
   }
 }
